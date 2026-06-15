fcf181749c5f3851d88c6d7200a4a76f1db077a7
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:34:32 +0530

    updated files

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+
+//ADDED NEW FEATURES IN JS FILE
+function searchBook() {
+
+    const searchText =
+        document.getElementById("searchBook")
+        .value
+        .toLowerCase();
+
+    const books =
+        document.querySelectorAll(".book");
+
+    books.forEach(book => {
+
+        if (
+            book.innerText
+                .toLowerCase()
+                .includes(searchText)
+        ) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
+
+    });
+}
