79ae62989db8fedd9361ca3cf5a1376981fb30ef
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:59:28 +0530

    updated files

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+
+//Update addBook():
+
+//Prevent empty book names.
+
+function addBook() {
+
+    const bookName =
+        document.getElementById("bookName").value;
+
+    if (bookName.trim() === "") {
+
+        alert("Book name cannot be empty");
+
+        return;
+    }
+
+    books.push(bookName);
+
+    displayBooks();
+}
