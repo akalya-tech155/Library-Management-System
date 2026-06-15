2e5e51f9763611d65931083a173c5f6e801b5c98
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:46:19 +0530

    updated files

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+
+//UPDATE SCRIPT.JS
+
+
+function updateBookCount() {
+
+    document.getElementById("bookCount")
+        .innerText =
+        `Total Books: ${books.length}`;
+}
