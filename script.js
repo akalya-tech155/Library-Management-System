d284665ad9cdbe34fa21733212cc244477ec1719
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:37:24 +0530

    modifying files

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+// UPDATED JS FILE
+
+function displayBooks() {
+
+    let output = "";
+
+    books.forEach(book => {
+
+        output += `
+        <div class="book">
+            ${book}
+            <button
+                class="borrow-btn"
+                onclick="borrowBook('${book}')">
+                Borrow
+            </button>
+        </div>`;
+    });
+
+    document.getElementById("bookList").innerHTML = output;
+}
