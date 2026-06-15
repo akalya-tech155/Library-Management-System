1d5020f04fdc6edca0944fdb83e7267bc73bbc5d
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:29:08 +0530

    updated js file

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+
+
+//ADDED NEW FEATURES IN JAVASCRIPT
+
+let books = [];
+
+function addBook() {
+
+    const bookName =
+        document.getElementById("bookName").value;
+
+    books.push(bookName);
+
+    displayBooks();
+}
+
+function displayBooks() {
+
+    let output = "";
+
+    books.forEach(book => {
+        output += `<div class="book">${book}</div>`;
+    });
+
+    document.getElementById("bookList").innerHTML = output;
+}
