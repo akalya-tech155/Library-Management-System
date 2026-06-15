3e83b7d4dbee2aa000e1dba09dedd1aa23b5c373
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-15 14:40:13 +0530

    modifying files

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ let books = [];
 function addBook() {
     console.log("Add Book");
 }
+
+// UPDATED JS FILE
+
+output += `
+<div class="book">
+    ${book}
+
+    <button
+        class="borrow-btn"
+        onclick="borrowBook('${book}')">
+        Borrow
+    </button>
+
+    <button
+        class="return-btn"
+        onclick="returnBook('${book}')">
+        Return
+    </button>
+
+</div>`;
